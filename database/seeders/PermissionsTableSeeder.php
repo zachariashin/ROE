be96932b1069e9b552e3bb638bb44aa683dcdeaa
Laravel Daily <info@laraveldaily.com>
2022-04-11 16:53:58 +0000

    QuickAdminPanel automatic commit

---
+++ database/seeders/PermissionsTableSeeder.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class PermissionsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $permissions = [
+            [
+                'id'    => 1,
+                'title' => 'user_management_access',
+            ],
+            [
+                'id'    => 2,
+                'title' => 'permission_create',
+            ],
+            [
+                'id'    => 3,
+                'title' => 'permission_edit',
+            ],
+            [
+                'id'    => 4,
+                'title' => 'permission_show',
+            ],
+            [
+                'id'    => 5,
+                'title' => 'permission_delete',
+            ],
+            [
+                'id'    => 6,
+                'title' => 'permission_access',
+            ],
+            [
+                'id'    => 7,
+                'title' => 'role_create',
+            ],
+            [
+                'id'    => 8,
+                'title' => 'role_edit',
+            ],
+            [
+                'id'    => 9,
+                'title' => 'role_show',
+            ],
+            [
+                'id'    => 10,
+                'title' => 'role_delete',
+            ],
+            [
+                'id'    => 11,
+                'title' => 'role_access',
+            ],
+            [
+                'id'    => 12,
+                'title' => 'user_create',
+            ],
+            [
+                'id'    => 13,
+                'title' => 'user_edit',
+            ],
+            [
+                'id'    => 14,
+                'title' => 'user_show',
+            ],
+            [
+                'id'    => 15,
+                'title' => 'user_delete',
+            ],
+            [
+                'id'    => 16,
+                'title' => 'user_access',
+            ],
+            [
+                'id'    => 17,
+                'title' => 'asset_management_access',
+            ],
+            [
+                'id'    => 18,
+                'title' => 'asset_category_create',
+            ],
+            [
+                'id'    => 19,
+                'title' => 'asset_category_edit',
+            ],
+            [
+                'id'    => 20,
+                'title' => 'asset_category_show',
+            ],
+            [
+                'id'    => 21,
+                'title' => 'asset_category_delete',
+            ],
+            [
+                'id'    => 22,
+                'title' => 'asset_category_access',
+            ],
+            [
+                'id'    => 23,
+                'title' => 'asset_location_create',
+            ],
+            [
+                'id'    => 24,
+                'title' => 'asset_location_edit',
+            ],
+            [
+                'id'    => 25,
+                'title' => 'asset_location_show',
+            ],
+            [
+                'id'    => 26,
+                'title' => 'asset_location_delete',
+            ],
+            [
+                'id'    => 27,
+                'title' => 'asset_location_access',
+            ],
+            [
+                'id'    => 28,
+                'title' => 'asset_status_create',
+            ],
+            [
+                'id'    => 29,
+                'title' => 'asset_status_edit',
+            ],
+            [
+                'id'    => 30,
+                'title' => 'asset_status_show',
+            ],
+            [
+                'id'    => 31,
+                'title' => 'asset_status_delete',
+            ],
+            [
+                'id'    => 32,
+                'title' => 'asset_status_access',
+            ],
+            [
+                'id'    => 33,
+                'title' => 'asset_create',
+            ],
+            [
+                'id'    => 34,
+                'title' => 'asset_edit',
+            ],
+            [
+                'id'    => 35,
+                'title' => 'asset_show',
+            ],
+            [
+                'id'    => 36,
+                'title' => 'asset_delete',
+            ],
+            [
+                'id'    => 37,
+                'title' => 'asset_access',
+            ],
+            [
+                'id'    => 38,
+                'title' => 'assets_history_access',
+            ],
+            [
+                'id'    => 39,
+                'title' => 'audit_log_show',
+            ],
+            [
+                'id'    => 40,
+                'title' => 'audit_log_access',
+            ],
+            [
+                'id'    => 41,
+                'title' => 'task_management_access',
+            ],
+            [
+                'id'    => 42,
+                'title' => 'task_status_create',
+            ],
+            [
+                'id'    => 43,
+                'title' => 'task_status_edit',
+            ],
+            [
+                'id'    => 44,
+                'title' => 'task_status_show',
+            ],
+            [
+                'id'    => 45,
+                'title' => 'task_status_delete',
+            ],
+            [
+                'id'    => 46,
+                'title' => 'task_status_access',
+            ],
+            [
+                'id'    => 47,
+                'title' => 'task_tag_create',
+            ],
+            [
+                'id'    => 48,
+                'title' => 'task_tag_edit',
+            ],
+            [
+                'id'    => 49,
+                'title' => 'task_tag_show',
+            ],
+            [
+                'id'    => 50,
+                'title' => 'task_tag_delete',
+            ],
+            [
+                'id'    => 51,
+                'title' => 'task_tag_access',
+            ],
+            [
+                'id'    => 52,
+                'title' => 'task_create',
+            ],
+            [
+                'id'    => 53,
+                'title' => 'task_edit',
+            ],
+            [
+                'id'    => 54,
+                'title' => 'task_show',
+            ],
+            [
+                'id'    => 55,
+                'title' => 'task_delete',
+            ],
+            [
+                'id'    => 56,
+                'title' => 'task_access',
+            ],
+            [
+                'id'    => 57,
+                'title' => 'tasks_calendar_access',
+            ],
+            [
+                'id'    => 58,
+                'title' => 'profile_password_edit',
+            ],
+        ];
+
+        Permission::insert($permissions);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,87 @@
+<?php
+
+Route::redirect('/', '/login');
+Route::get('/home', function () {
+    if (session('status')) {
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+
+    return redirect()->route('admin.home');
+});
+
+Auth::routes(['register' => false]);
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    // Permissions
+    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
+    Route::resource('permissions', 'PermissionsController');
+
+    // Roles
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+    Route::resource('roles', 'RolesController');
+
+    // Users
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+    Route::resource('users', 'UsersController');
+
+    // Asset Category
+    Route::delete('asset-categories/destroy', 'AssetCategoryController@massDestroy')->name('asset-categories.massDestroy');
+    Route::resource('asset-categories', 'AssetCategoryController');
+
+    // Asset Location
+    Route::delete('asset-locations/destroy', 'AssetLocationController@massDestroy')->name('asset-locations.massDestroy');
+    Route::resource('asset-locations', 'AssetLocationController');
+
+    // Asset Status
+    Route::delete('asset-statuses/destroy', 'AssetStatusController@massDestroy')->name('asset-statuses.massDestroy');
+    Route::resource('asset-statuses', 'AssetStatusController');
+
+    // Asset
+    Route::delete('assets/destroy', 'AssetController@massDestroy')->name('assets.massDestroy');
+    Route::post('assets/media', 'AssetController@storeMedia')->name('assets.storeMedia');
+    Route::post('assets/ckmedia', 'AssetController@storeCKEditorImages')->name('assets.storeCKEditorImages');
+    Route::resource('assets', 'AssetController');
+
+    // Assets History
+    Route::resource('assets-histories', 'AssetsHistoryController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
+
+    // Audit Logs
+    Route::resource('audit-logs', 'AuditLogsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
+
+    // Task Status
+    Route::delete('task-statuses/destroy', 'TaskStatusController@massDestroy')->name('task-statuses.massDestroy');
+    Route::resource('task-statuses', 'TaskStatusController');
+
+    // Task Tag
+    Route::delete('task-tags/destroy', 'TaskTagController@massDestroy')->name('task-tags.massDestroy');
+    Route::resource('task-tags', 'TaskTagController');
+
+    // Task
+    Route::delete('tasks/destroy', 'TaskController@massDestroy')->name('tasks.massDestroy');
+    Route::post('tasks/media', 'TaskController@storeMedia')->name('tasks.storeMedia');
+    Route::post('tasks/ckmedia', 'TaskController@storeCKEditorImages')->name('tasks.storeCKEditorImages');
+    Route::resource('tasks', 'TaskController');
+
+    // Tasks Calendar
+    Route::resource('tasks-calendars', 'TasksCalendarController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
+
+    Route::get('messenger', 'MessengerController@index')->name('messenger.index');
+    Route::get('messenger/create', 'MessengerController@createTopic')->name('messenger.createTopic');
+    Route::post('messenger', 'MessengerController@storeTopic')->name('messenger.storeTopic');
+    Route::get('messenger/inbox', 'MessengerController@showInbox')->name('messenger.showInbox');
+    Route::get('messenger/outbox', 'MessengerController@showOutbox')->name('messenger.showOutbox');
+    Route::get('messenger/{topic}', 'MessengerController@showMessages')->name('messenger.showMessages');
+    Route::delete('messenger/{topic}', 'MessengerController@destroyTopic')->name('messenger.destroyTopic');
+    Route::post('messenger/{topic}/reply', 'MessengerController@replyToTopic')->name('messenger.reply');
+    Route::get('messenger/{topic}/reply', 'MessengerController@showReply')->name('messenger.showReply');
+});
+Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 'middleware' => ['auth']], function () {
+    // Change password
+    if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))) {
+        Route::get('password', 'ChangePasswordController@edit')->name('password.edit');
+        Route::post('password', 'ChangePasswordController@update')->name('password.update');
+        Route::post('profile', 'ChangePasswordController@updateProfile')->name('password.updateProfile');
+        Route::post('profile/destroy', 'ChangePasswordController@destroy')->name('password.destroyProfile');
+    }
+});
